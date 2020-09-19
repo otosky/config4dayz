e6d4a10c7b9a455c326550b11e618273248da75e
Oliver Tosky <olivertosky@Olivers-MacBook-Pro.local>
2020-09-19 13:05:27 -0400

    Add vscode command

--- zsh-edit.sh
+++ zsh-edit.sh
@@ -10,3 +10,9 @@ git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/p
 sed -i 's/plugins=(.+)/plugins=(git zsh-autosuggestions zsh-syntax-highlighting)/g' .zshrc 
 
 echo "export DIRENV_LOG_FORMAT=" >> .zshrc
+
+# add VSCode command
+cat << EOF >> ~/.zshrc
+# Add Visual Studio Code (code)
+export PATH="/Applications/Visual Studio Code.app/Contents/Resources/app/bin:$PATH"
+EOF
